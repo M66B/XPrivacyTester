37388b6e0c3e7b106a8c5701ee807e6280efaef7
M66B <marcel@faircode.eu>
2014-07-26 11:39:17 +0200

    Add some of the package manager methods

--- src/biz/bokhorst/xprivacytester/MainActivity.java
+++ src/biz/bokhorst/xprivacytester/MainActivity.java
@@ -4,6 +4,7 @@ import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -15,12 +16,6 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		AccountManager accountManager = (AccountManager) getSystemService(ACCOUNT_SERVICE);
-		((TextView) findViewById(R.id.getAccounts)).setText(Integer
-				.toString(accountManager.getAccounts().length));
-		((TextView) findViewById(R.id.getCurrentSyncs)).setText(Integer
-				.toString(ContentResolver.getCurrentSyncs().size()));
 
 		Intent call = new Intent(Intent.ACTION_CALL);
 		call.setData(Uri.parse("tel:911"));
@@ -42,5 +37,21 @@ public class MainActivity extends Activity {
 		Intent view = new Intent(Intent.ACTION_VIEW);
 		view.setData(Uri.parse("http://www.faircode.eu/"));
 		startActivity(view);
+
+		AccountManager accountManager = (AccountManager) getSystemService(ACCOUNT_SERVICE);
+		((TextView) findViewById(R.id.getAccounts)).setText(Integer
+				.toString(accountManager.getAccounts().length));
+		((TextView) findViewById(R.id.getCurrentSyncs)).setText(Integer
+				.toString(ContentResolver.getCurrentSyncs().size()));
+
+		PackageManager packageManager = getPackageManager();
+		((TextView) findViewById(R.id.getInstalledApplications))
+				.setText(Integer.toString(packageManager
+						.getInstalledApplications(0).size()));
+		((TextView) findViewById(R.id.getInstalledPackages)).setText(Integer
+				.toString(packageManager.getInstalledPackages(0).size()));
+		((TextView) findViewById(R.id.queryIntentActivities))
+				.setText(Integer.toString(packageManager.queryIntentActivities(
+						view, 0).size()));
 	}
 }
