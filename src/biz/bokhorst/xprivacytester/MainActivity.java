12a9b653c231f6da326b7d04c643c27f2f5c60c2
M66B <marcel@faircode.eu>
2014-07-27 10:08:45 +0200

    Add CalendarProvider2

--- src/biz/bokhorst/xprivacytester/MainActivity.java
+++ src/biz/bokhorst/xprivacytester/MainActivity.java
@@ -8,8 +8,8 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract.Calendars;
 import android.provider.ContactsContract;
-import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -19,16 +19,19 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		// View action
 		Intent view = new Intent(Intent.ACTION_VIEW);
 		view.setData(Uri.parse("http://www.faircode.eu/"));
 		startActivity(view);
 
+		// Acount manager methods
 		AccountManager accountManager = (AccountManager) getSystemService(ACCOUNT_SERVICE);
 		((TextView) findViewById(R.id.getAccounts)).setText(Integer
 				.toString(accountManager.getAccounts().length));
 		((TextView) findViewById(R.id.getCurrentSyncs)).setText(Integer
 				.toString(ContentResolver.getCurrentSyncs().size()));
 
+		// Package manager methods
 		PackageManager packageManager = getPackageManager();
 		((TextView) findViewById(R.id.getInstalledApplications))
 				.setText(Integer.toString(packageManager
@@ -39,10 +42,22 @@ public class MainActivity extends Activity {
 				.setText(Integer.toString(packageManager.queryIntentActivities(
 						view, 0).size()));
 
-		Cursor cursor = getContentResolver().query(
-				ContactsContract.Contacts.CONTENT_URI,
+		Cursor cursor;
+		ContentResolver cr = getContentResolver();
+
+		// Calendar provider
+		cursor = cr.query(Calendars.CONTENT_URI,
+				new String[] { Calendars._ID }, null, null, null);
+		((TextView) findViewById(R.id.CalendarProvider2))
+				.setText(cursor == null ? "null" : Integer.toString(cursor
+						.getCount()));
+		if (cursor != null)
+			cursor.close();
+
+		// Contacts provider
+		cursor = cr.query(ContactsContract.Contacts.CONTENT_URI,
 				new String[] { ContactsContract.Contacts._ID }, null, null,
-				Phone.DISPLAY_NAME);
+				null);
 		((TextView) findViewById(R.id.ContactsProvider2))
 				.setText(cursor == null ? "null" : Integer.toString(cursor
 						.getCount()));
